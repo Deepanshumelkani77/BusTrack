57fd344e6c633ece60c6dbd4fd3a2a06c25609ad
Deepanshumelkani77 <deepumelkani123@gmail.com>
2026-04-20 16:02:19 +0530

    database connected successfully

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const mongoose = require('mongoose');
 const connectDB= async()=>{
     try{
       await mongoose.connect(
-"mongodb+srv://deepumelkani123_db_user:Dev7777@cluster0.t5m5dsp.mongodb.net/?appName=Cluster0"
+"mongodb://deepumelkani123_db_user:Dev7777@ac-ghsi0ai-shard-00-00.t5m5dsp.mongodb.net:27017,ac-ghsi0ai-shard-00-01.t5m5dsp.mongodb.net:27017,ac-ghsi0ai-shard-00-02.t5m5dsp.mongodb.net:27017/?ssl=true&replicaSet=atlas-1usy7g-shard-0&authSource=admin&appName=Cluster0"
 );
 
         console.log("Connected to MongoDB");
