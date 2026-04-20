3325ff9e85f9d39147bb572ee85733bb37b34cdf
Deepanshumelkani77 <deepumelkani123@gmail.com>
2026-04-20 15:52:45 +0530

    try to set backend

--- backend/index.js
+++ backend/index.js
@@ -10,3 +10,20 @@ app.use(cors());
 app.use(express.json());
 
 
+
+//mongoose
+const mongoose = require('mongoose');
+const connectDB= async()=>{
+    try{
+      await mongoose.connect(
+"mongodb+srv://deepumelkani123_db_user:Dev7777@cluster0.t5m5dsp.mongodb.net/?appName=Cluster0"
+);
+
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+connectDB();
+//Vc3xdfgLbMKYt4ko
