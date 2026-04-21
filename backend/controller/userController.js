1704987f182cf32afbf84f126f431ee9de1e68ed
Deepanshumelkani77 <deepumelkani123@gmail.com>
2026-04-21 21:11:46 +0530

    try to make controller

---
+++ backend/controller/userController.js
@@ -0,0 +1,14 @@
+const User=require('../models/User');
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+
+
+exports.signupUser=async(req,res)=>{
+
+
+}
+
+exports.loginUser=async(req,res)=>{
+
+
+}
