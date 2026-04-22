3238f9799d7df80754d74143c5b64063f9a67599
Deepanshumelkani77 <deepumelkani123@gmail.com>
2026-04-22 19:45:15 +0530

    added more images

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,54 @@
+import i1 from "./i1.png";
+import i2 from "./i2.png";
+import i3 from "./i3.png";
+
+import delhi from "./delhi.jpg";
+import mumbai from "./mumbai.jpg";
+import haldwani from "./haldwani.jpg";
+import kochi from "./kochi.jpg";
+import varanasi from "./varanasi.jpg";
+import ayodhya from "./ayodhya.jpg";
+import jaipur from "./jaipur.jpg";
+import goa from "./goa.jpg";
+import muradabad from "./muradabad.jpg";
+import banglore from "./banglore.jpg";
+import ahmdabad from "./ahmdabad.jpg";
+import haridwar from "./haridwar.jpg";
+import aagra from "./aagra.jpg";
+import dehradun from "./dehradun.jpg";
+import patna from "./patna.jpg";
+import jodhpur from "./jodhpur.jpg";
+import lucknow from "./lucknow.jpg";
+import indore from "./indore.jpg";
+import punjab from "./punjab.jpg";
+import chennai from "./chennai.jpg";
+
+
+export const assets = {
+ i1,
+    i2,
+    i3,
+        delhi,
+    mumbai,
+    haldwani,
+    kochi,
+    varanasi,
+    ayodhya,
+    jaipur,
+    goa,
+    muradabad,
+    banglore,
+    ahmdabad,
+    haridwar,
+    aagra,
+    dehradun,
+    patna,
+    jodhpur,
+    lucknow,
+    indore,
+    punjab,
+    chennai
+
+}
+
+
