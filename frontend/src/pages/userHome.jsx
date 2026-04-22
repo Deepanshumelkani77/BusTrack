5d03ff9cfe541cb7075d66690b37d9de9cf82323
Deepanshumelkani77 <deepumelkani123@gmail.com>
2026-04-22 19:53:57 +0530

    home creating

---
+++ frontend/src/pages/userHome.jsx
@@ -0,0 +1,23 @@
+
+import Header from '../components/Header'
+import Why from '../components/Why'
+
+import Slider from '../components/Slider'
+import City from '../components/City'
+
+
+const userHome = () => {
+  return (
+    <div>
+      <Header />
+      <Why />
+     
+      <Slider />
+      <City/>
+    
+      
+    </div>
+  )
+}
+
+export default userHome
