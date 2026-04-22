a21a976e88ec72e4ff60470ab25a74202f3a9ad1
Deepanshumelkani77 <deepumelkani123@gmail.com>
2026-04-22 20:35:25 +0530

    all done

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -22,6 +22,12 @@ import lucknow from "./lucknow.jpg";
 import indore from "./indore.jpg";
 import punjab from "./punjab.jpg";
 import chennai from "./chennai.jpg";
+import d1 from "./d1.jpg";
+import d2 from "./d2.jpg";
+import d3 from "./d3.jpg";
+import d4 from "./d4.jpg";
+import d5 from "./d5.jpg";
+import d6 from "./d6.jpg";
 
 
 export const assets = {
@@ -47,7 +53,13 @@ export const assets = {
     lucknow,
     indore,
     punjab,
-    chennai
+    chennai,
+     d1,
+    d2,
+    d3,
+    d4,
+    d5,
+    d6,
 
 }
 
