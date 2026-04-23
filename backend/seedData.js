5d38c89599c1db6075dc5bacdfdbe9102e596829
Deepanshumelkani77 <deepumelkani123@gmail.com>
2026-04-23 09:08:47 +0530

    try to make seeData.js

---
+++ backend/seedData.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+const mongoURI="mongodb://deepumelkani123_db_user:Dev7777@ac-ghsi0ai-shard-00-00.t5m5dsp.mongodb.net:27017,ac-ghsi0ai-shard-00-01.t5m5dsp.mongodb.net:27017,ac-ghsi0ai-shard-00-02.t5m5dsp.mongodb.net:27017/?ssl=true&replicaSet=atlas-1usy7g-shard-0&authSource=admin&appName=Cluster0";
+
+
